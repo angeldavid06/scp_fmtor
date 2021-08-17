1f0ba6e0102733943208460be65c59e03130fcf6
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-17 11:17:05 -0500

    2021-08-17 11:16 am Implementación de alertas en el login

---
+++ public/js/alert.js
@@ -0,0 +1,56 @@
+const render_titulo = (titulo) => {
+    const h3 = document.createElement('h3');
+    h3.classList.add('titulo');
+    h3.innerHTML = titulo;
+    return h3;
+}
+
+const render_descripcion = (descripcion) => {
+    const p = document.createElement('p');
+    p.classList.add('descripcion');
+    p.innerHTML = descripcion;
+    return p;
+}
+
+const render_contenedor = () => {
+    const div = document.createElement('div');
+    div.classList.add('contenido');
+    return div;
+}
+
+const render_alert_con = (color) => {
+    const div = document.createElement('div');
+    div.classList.add('alert');
+    div.classList.add('alert-'+color);
+    return div;
+}
+
+const render_alert = (titulo, descripcion, color) => {
+    const t = render_titulo(titulo);
+    const d = render_descripcion(descripcion);
+    const c = render_contenedor();
+    const a = render_alert_con(color);
+    c.appendChild(t)
+    c.appendChild(d)
+    a.appendChild(c)
+    document.body.appendChild(a)
+    
+    window.setTimeout(() => {
+        a.classList.add('show-alert')
+    },300);
+
+    window.setTimeout(() => {
+        a.classList.remove('show-alert')
+    },4000);
+    
+    window.setTimeout(() => {  
+        document.body.removeChild(a)
+    },4300);
+}
+
+{/* <div class="alert alert-rojo">
+    <div class="contenido">
+        <h3 class="titulo">Titulo:</h3>
+        <p class="descripcion">Descripción del problema</p>
+    </div>
+</div> */}
--- public/js/login.js
+++ public/js/login.js
@@ -5,3 +5,24 @@ if (document.getElementsByClassName('btn-login-form')) {
         row[0].classList.toggle('hidden');
     });
 }
+
+const form = document.getElementById('form-login');
+
+form.addEventListener('submit', (evt) => {
+    evt.preventDefault();
+    const usu = document.getElementById('nombre_usuario');
+    const pass = document.getElementById('password');
+    usu.classList.remove('input-error')
+    pass.classList.remove('input-error')
+    if (usu.value == '' && pass.value == '') {
+        usu.classList.add('input-error')
+        pass.classList.add('input-error')
+        render_alert('Error al iniciar sesión:','No ha introducido el nombre de usuario o contraseña','rojo');
+    } else if (pass.value == '') {
+        pass.classList.add('input-error')
+        render_alert('Error al iniciar sesión:','No ha introducido la contraseña','rojo');
+    } else if (usu.value == '') {
+        usu.classList.add('input-error')
+        render_alert('Error al iniciar sesión:','No ha introducido el nombre de usuario','rojo');
+    }
+});
