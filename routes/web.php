11edac790ae1afd58e4e322f60f431fe339fe08a
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-17 20:37:27 -0500

    2021-08-17 08:37 pm Creación del archivo Web para las rutas de las vistas

---
+++ routes/web.php
@@ -0,0 +1,11 @@
+<?php 
+
+    class Web {
+        public function View ($view,$data) {
+            if (file_exists('views/'.$view.'.php')) {
+                return require_once 'views/'.$view.'.php';
+            }
+        }
+    }
+
+?>
