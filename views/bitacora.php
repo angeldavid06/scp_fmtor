4d2ee9fad04d129dd5e9cec09763c7519f1dfac4
angeldavid06 <mendozaangel.3243@gmail.com>
2021-08-24 11:31:15 -0500

    2021-08-24 11:30 am Creación de un modulo del head

--- views/bitacora.php
+++ views/bitacora.php
@@ -2,12 +2,8 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php require_once 'public/componentes/head.php'; ?>
     <title>Bitácora</title>
-    <link rel="stylesheet" href="public/css/estilos.css">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
 <body>
     <div class="main">
