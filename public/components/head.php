d4a74a7dde1a78faa2968dcf85c039e43b2ff701
angeldavid06 <mendozaangel.3243@gmail.com>
2021-09-15 11:00:35 -0500

    2021-09-15 11:00 am Nueva tabla y cambio en las vistas de la base de datos

--- public/components/head.php
+++ public/components/head.php
@@ -2,5 +2,5 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <!-- <link rel="stylesheet" href="public/css/estilos.css?1.7"> -->
-<link rel="stylesheet" href="public/css/styles.css?5.4">
+<link rel="stylesheet" href="public/css/styles.css?5.6">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
